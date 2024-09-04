b63ad086326cf057d0f51a1394df26cef1a070d4
Sasha Fetisov <sasafetisov03@gmail.com>
2024-09-04 22:53:06 +0300

    SpringBoot Intro fixed task implemeted by fetis_off

--- src/main/java/mate/academy/springbootintro/repository/BookRepositoryImpl.java
+++ src/main/java/mate/academy/springbootintro/repository/BookRepositoryImpl.java
@@ -1,21 +1,17 @@
 package mate.academy.springbootintro.repository;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mate.academy.springbootintro.model.Book;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 
     @Override
     public Book save(Book book) {
@@ -42,10 +38,8 @@ public class BookRepositoryImpl implements BookRepository {
     @Override
     public List<Book> findAll() {
         List<Book> books;
-        Session session = null;
         Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             books = session.createQuery("from Book ").list();
             transaction.commit();
@@ -55,10 +49,6 @@ public class BookRepositoryImpl implements BookRepository {
                 transaction.rollback();
             }
             throw new RuntimeException("Cant find all books", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 }
